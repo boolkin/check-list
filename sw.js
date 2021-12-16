825df0c5edb21adc5561d80c0f738948f560fc84
boolkin <boolkin@gmail.com>
2021-12-16 09:05:38 +0300

    sw,js

--- sw.js
+++ sw.js
@@ -1,35 +1,39 @@
-'use strict';
-const CACHE_STATIC = 'static-cache-v1';
-        const files = [
-            './',
-            './manifest.json',
-            './images/240x240.png',
-            './index.html',
-            './sw.js',
-        ];
+const cacheName = 'check-list';
+const contentToCache = [
+  '/check-list/',
+  '/check-list/index.html',
+  '/check-list/sw.js',
+  '/check-list/style/main.css',
+  '/check-list/manifest.json',
+  '/check-list/images/240x240.png'
+];
 
-
-function hndlEventFetch(evt) {
-    async function getFromCache() {
-        const cache = await self.caches.open(CACHE_STATIC);
-        const cachedResponse = await cache.match(evt.request);
-        if (cachedResponse) {
-            return cachedResponse;
-        }
-        // wait until resource will be fetched from server and stored in cache
-        const resp = await fetch(evt.request);
-        await cache.put(evt.request, resp.clone());
-        return resp;
-    }
-
-    evt.respondWith(getFromCache());
-}
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_STATIC).then(cache => {
-      return cache.addAll(files);
-    })
-  );
+// Install the Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(contentToCache);
+  })());
 });
-self.addEventListener('fetch', hndlEventFetch);
+
+// Fetch content using the Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+// Clear the cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
+});
